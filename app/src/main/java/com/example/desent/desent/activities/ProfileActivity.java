145a17d936a802653eeab201d682aef7b6f5fe48
ragnhlar <ragnhlar@stud.ntnu.no>
2018-03-14 13:42:43 +0100

    added side bar to profile activity

--- app/src/main/java/com/example/desent/desent/activities/ProfileActivity.java
+++ app/src/main/java/com/example/desent/desent/activities/ProfileActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -14,28 +15,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.desent.desent.R;
 import com.example.desent.desent.SessionManagement;
-import com.example.desent.desent.models.DatabaseHelper;
-import com.example.desent.desent.models.Energy;
-import com.example.desent.desent.models.Transportation;
 import com.example.desent.desent.utils.Utility;
-import com.example.desent.desent.views.StackBarChart;
-import com.example.desent.desent.views.StackedBarLabel;
-import com.example.desent.desent.views.Yaxis;
-
 import java.io.FileNotFoundException;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity
+    implements NavigationView.OnNavigationItemSelectedListener{
 
     SharedPreferences sharedPreferences;
     TextView name, email, address, zipcode, city, birthdate, gender;
     ImageView changeInfo, profilePic;
+    DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +37,16 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        setUpNavigationView();
 
         changeInfo = findViewById(R.id.changeUserInfo);
         profilePic = findViewById(R.id.imgProfilePicture);
@@ -68,5 +72,65 @@ public class ProfileActivity extends AppCompatActivity {
 
         gender = findViewById(R.id.tvGender);
         gender.setText(sharedPreferences.getString("pref_key_personal_gender",""));
+    }
+
+    protected void setUpNavigationView(){
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ((TextView) navigationView.getHeaderView(0).findViewById(R.id.title_nav_header)).setText(PreferenceManager.getDefaultSharedPreferences(this).getString("pref_key_personal_name", ""));
+        ((TextView) navigationView.getHeaderView(0).findViewById(R.id.subtitle_nav_header)).setText(PreferenceManager.getDefaultSharedPreferences(this).getString("pref_key_personal_email", ""));
+        ImageView profilePicture = navigationView.getHeaderView(0).findViewById(R.id.imageView);
+
+        Uri imageUri = null;
+        try {
+            imageUri = Uri.parse(PreferenceManager.getDefaultSharedPreferences(this).getString("pref_key_profile_picture", "android.resource://com.example.desent.desent/drawable/earth"));
+        } catch (IllegalArgumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        Bitmap bitmap;
+        try {
+            bitmap = BitmapFactory.decodeStream(this.getContentResolver().openInputStream(imageUri));
+            profilePicture.setImageBitmap(Utility.getCroppedBitmap(bitmap));
+            bitmap.recycle();
+            bitmap = null;
+            System.gc();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+            drawer.closeDrawer(GravityCompat.START);
+        } else if (id == R.id.nav_history) {
+            startActivity(new Intent(ProfileActivity.this, HistoryActivity.class));
+            drawer.closeDrawer(GravityCompat.START);
+        } else if (id == R.id.nav_settings) {
+            startActivity(new Intent(ProfileActivity.this, SettingsActivity.class));
+            drawer.closeDrawer(GravityCompat.START);
+        } else if (id == R.id.nav_user_profile) {
+            //startActivity(new Intent(ProfileActivity.this, ProfileActivity.class));
+            drawer.closeDrawer(GravityCompat.START);
+        } else if (id == R.id.nav_info_app) {
+            startActivity(new Intent(ProfileActivity.this, InformationActivity.class));
+            drawer.closeDrawer(GravityCompat.START);
+        } else if (id == R.id.nav_log_out){
+            SessionManagement session = new SessionManagement(getApplicationContext());
+            session.logoutUser();
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        /*else if (id == R.id.nav_about_us) {
+            drawer.closeDrawer(GravityCompat.START);
+        }*/
+        return true;
     }
 }
